43e5a1952b1f940b567271318d80b196f35b9ed0
mendoncinha <rebs.mbm003@gmail.com>
2025-03-10 14:01:14 -0300

    fix: correction of authentication routes.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
-from app.modules.auth.auth_controller import router as login, register
+from app.modules.auth.auth_controller import router as auth_router
 
 router = APIRouter()
 
-router.include_router(login)
-router.include_router(register)
+router.include_router(auth_router)
